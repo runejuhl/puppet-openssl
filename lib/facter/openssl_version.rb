ffaed8145b7fb969f6257acfc0495b5ec9711183
Rune Juhl Jacobsen <runejuhl@petardo.dk>
2020-03-16 20:14:46 +0100

    Add structured fact

--- lib/facter/openssl_version.rb
+++ lib/facter/openssl_version.rb
@@ -1,9 +1,25 @@
-Facter.add(:openssl_version) do
-  setcode do
-    if Facter::Util::Resolution.which('openssl')
-      openssl_version = Facter::Util::Resolution.exec('openssl version 2>&1')
-      matches = %r{^OpenSSL ([\w\.\-]+)( FIPS)?([ ]+)([\d\.]+)([ ]+)([\w\.]+)([ ]+)([\d\.]+)}.match(openssl_version)
-      matches[1] if matches
+openssl_version_raw = if Facter::Util::Resolution.which('openssl')
+                        Facter::Util::Resolution.exec('openssl version 2>&1')
+                      end
+
+if openssl_version_raw
+  matches = %r{^OpenSSL ([\w\.\-]+)( FIPS)?([ ]+)([\d\.]+)([ ]+)([\w\.]+)([ ]+)([\d\.]+)}.match(openssl_version_raw)
+  version = matches[1] if matches
+
+  Facter.add(:openssl_version) do
+    setcode do
+      version
     end
   end
+
+  Facter.add(:openssl, type: :aggregate) do
+    chunk(:fips) do
+      { fips: !!openssl_version_raw.match(/\Wfips\W/i)}
+    end
+
+    chunk(:version) do
+      version
+    end
+  end
+
 end
